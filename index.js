efd6fb0bb68ee641c6ef7ea3cd1fdad486b428ef
Azat Mardanov <azat.mardanov@gmail.com>
2013-03-29 13:37:41 -0700

    list of databases

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+var mongo = require('mongodb');
 var express = require('express');
 var monk = require('monk');
 var db =  monk('localhost:27017/test');
 var app = new express();
+  console.log();
 
 app.use(express.static(__dirname + '/public'));
 app.get('/',function(req,res){
-  res.json({});
-})
+  db.driver.admin.listDatabases(function(e,dbs){
+      res.json(dbs);
+  });
+});
+app.get('/collections',function(req,res){
+  db.driver.collectionNames(function(e,names){
+    res.json(names);
+  })
+});
 app.get('/collections/:name',function(req,res){
   var collection = db.get(req.params.name);
   collection.find({},{limit:20},function(e,docs){
---
+++ readme.md
@@ -0,0 +1,22 @@
+# MongoUI
+
+MongoUI (mongoui) is an web browser admin interface for MongoDB.
+
+## Why
+
+Tired of typing db.users.findOne and show collections. Can't find Node.js implementaton.
+
+# Use
+
+To start the server run:
+
+  $ node .
+
+or:
+
+  $ node index.js
+
+# Chrome extension
+
+Download and install [JSONView Chrome extension](https://chrome.google.com/webstore/detail/jsonview/chklaanhfefbnpoihckbnefhakgolnmc) to view JSON in a human readable format.
+
