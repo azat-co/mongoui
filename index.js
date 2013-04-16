71dcf22bcaa836bd98e8c062ed25f9753636d68e
Azat Mardanov <azat.mardanov@gmail.com>
2013-04-16 00:57:07 -0700

    hooked up derby, made database selector, updates via dropdown menu, show list of collection, open collection in a new window (json)

--- index.js
+++ index.js
@@ -1,26 +1,114 @@
+var http = require('http');
 var mongo = require('mongodb');
 var express = require('express');
 var monk = require('monk');
-var db =  monk('localhost:27017/test');
+var config = require('./config.json')
+var db = monk(config.database.
+default.host + ':' + config.database.
+default.port + '/' + config.database.
+default.name);
+// var db= monk('localhost:27017/test')
+var derby = require('derby');
+
 var app = new express();
-  console.log();
+// console.log(config.database.default.host+':'+config.database.default.port+'/' + config.database.default.name);
+var server = http.createServer(app)
+// console.log(derby.logPlugin)
+derby.use(derby.logPlugin)
+var store = derby.createStore({
+  listen: server
+});
+store.afterDb("set", "dbName", function(txn, doc, prevDoc, done) {
+  console.log(txn, txn.length, txn[3][1])
+  if (txn && (txn.length > 2) && txn[3][1]) {
+    console.log(txn[3][1]);
+    //filter dbName based on existing list
+    db = monk(config.database.
+    default.host + ':' + config.database.
+    default.port + '/' + txn[3][1]);
+    // model.set('dbName',txn[3].dbName);
+    // db.driver.admin.listDatabases(function(e,dbs){
+    // model.set('dbs',dbs);
+    db.driver.collectionNames(function(e, names) {
+      console.log(names)
+      model.set('collections', names);
+      model.subscribe('collections', function() {
+        done();
+
+      });
+      // derbyApp.page.redirect('main');
+    });
+    // });
+  }
+
+});
+var model = store.createModel();
+// console.log(Object.keys(store))
+// console.log(store.io)
+// io.sockets.on('connection',function(socket){
+// socket.on('set')
+// })
+// model.on('set',function(path,obj){
+// console.log('***',path,obj)
+// })
+
+
+// console.log(con)
+
+app.use(store.modelMiddleware());
+
+var derbyApp = require('./main');
+derbyApp.get('/main', function(page, model, params, next) {
+  model.set('dbName', config.database.default.name);
+  db.driver.admin.listDatabases(function(e, dbs) {
+    model.set('dbs', dbs);
+    db.driver.collectionNames(function(e, names) {
+      console.log(names)
+      model.set('collections', names);
+      model.subscribe('dbs', function() {
+        model.subscribe('collections', function() {
+          model.subscribe('dbName', function(e, dbName) {
+            // page.render({dbName:'test'});
+            page.render();
+            // return next();
+          });
+        });
+      });
+    });
+  });
+
+});
+
+// derbyApp.on('changeDatabase',function(obj,obj){
+// console.log('HAHAHA')
+// })
+app.use(derbyApp.router());
 
 app.use(express.static(__dirname + '/public'));
-app.get('/',function(req,res){
-  db.driver.admin.listDatabases(function(e,dbs){
-      res.json(dbs);
+// console.log(Object.keys(model.async))
+// console.log(model)
+app.get('/main', function(req, res) {
+  console.log(req.getModel().get('dbName'));
+});
+app.get('/api.json', function(req, res) {
+  db.driver.admin.listDatabases(function(e, dbs) {
+    res.json(dbs);
   });
 });
-app.get('/collections',function(req,res){
-  db.driver.collectionNames(function(e,names){
+app.get('/api/collections.json', function(req, res) {
+  db.driver.collectionNames(function(e, names) {
     res.json(names);
   });
 });
-app.get('/collections/:name',function(req,res){
+app.get('/api/collections/:db.:name.json', function(req, res) {
+  // console.log('!',db)
   var collection = db.get(req.params.name);
-  collection.find({},{limit:20},function(e,docs){
+  collection.find({}, {
+    limit: 20
+  }, function(e, docs) {
+    console.log('boo',docs)
     res.json(docs);
   });
 });
 
-app.listen(3000);
+server.listen(3000);
---
+++ main.js
@@ -0,0 +1,39 @@
+var app = require('derby').createApp(module);
+
+
+app.ready(function(model) {
+
+  // model.on('set', '*', function(path, object) {
+      // console.log('!***', path, object)
+  // });
+  model.on('set','dbName',function(path,object){
+    // console.log(object)
+    // console.log(app);
+    // app.emit('changeDatabase')
+    // app.page.render();
+    // app.page.render('main',{dbName:'object'});
+  });
+  model.on('set','collections',function(path,object){
+    console.log('!',model.get('collections'))
+    app.page.render();
+  })
+  app.changeDatabase = function(e, element, next) {
+    app.model.set('dbName', element.value);
+    // element.select();
+    console.log(app.model.get('dbName'));
+    next();
+  };
+
+})
+
+// app.get('/main', function(page, model,params,next) {
+//   model.subscribe('dbs', function() {
+//     model.subscribe('collections', function() {
+//       model.subscribe('dbName', function(e, dbName) {
+//         // page.render({dbName:'test'});
+//         page.render();
+//         // return next();
+//       });
+//     });
+//   });
+// });
