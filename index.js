999979d0e5dd4db1bb2096dd0f2543eba2fbecf7
Azat Mardanov <azat.mardanov@gmail.com>
2013-04-25 23:40:29 -0700

    first view-only version
    1. code clean-up
    1. added view of selected collection
    1. bootwatch dark responsive theme
    1. added links to github

--- index.js
+++ index.js
@@ -4,98 +4,114 @@ var async = require('async');
 var express = require('express');
 var monk = require('monk');
 var config = require('./config.json');
-var db = monk(config.database.
-default.host + ':' + config.database.
-default.port + '/' + config.database.
-default.name);
+var db = monk(config.database.default.host + 
+  ':' + 
+  config.database.default.port + 
+  '/' +
+  config.database.default.name);
+
 // var db= monk('localhost:27017/test')
 var derby = require('derby');
 
 var app = new express();
-// console.log(config.database.default.host+':'+config.database.default.port+'/' + config.database.default.name);
+
 var server = http.createServer(app);
-// console.log(derby.logPlugin)
+
 derby.use(derby.logPlugin);
 var store = derby.createStore({
   listen: server
 });
+var model = store.createModel();
+
 store.afterDb("set", "dbName", function(txn, doc, prevDoc, done) {
-  console.log(txn, txn.length, txn[3][1]);
+  if (txn && (txn.length > 2) && txn[3][1]) {
+    //filter dbName based on existing list
+    db = monk(config.database.default.host + ':' + config.database.default.port + '/' + txn[3][1]);
+    db.driver.collectionNames(function(e, names) {
+      //TODO: abstract it
+      names.forEach(function(el,i,list){
+        el.name=el.name.split('.')[1];
+      })
+      model.set('collections', names);
+      // model.subscribe('collections', function() {
+        done();
+      // });
+    });
+  }
+});
+
+
+
+store.afterDb("set", "collectionBoxName", function(txn, doc, prevDoc, done) {
   if (txn && (txn.length > 2) && txn[3][1]) {
     console.log(txn[3][1]);
-    //filter dbName based on existing list
-    db = monk(config.database.
-    default.host + ':' + config.database.
-    default.port + '/' + txn[3][1]);
-    // model.set('dbName',txn[3].dbName);
-    // db.driver.admin.listDatabases(function(e,dbs){
-    // model.set('dbs',dbs);
-    db.driver.collectionNames(function(e, names) {
-      console.log(names);
-      model.set('collections', names);
-      model.subscribe('collections', function() {
-        done();
-
-      });
-      // derbyApp.page.redirect('main');
-    });
-    // });
+    var collectionName = txn[3][1];
+    db.get(collectionName).find({}, {
+      limit: 20
+    }, function(e, items) {
+      if (e) console.error(e)
+      // db.get(txn[3][1]).find({},{$limit:20}, function (e, items){
+      model.set('collectionBox', JSON.stringify(items,0,2));
+      done()
+    })
   }
-
-});
-var model = store.createModel();
-// console.log(Object.keys(store))
-// console.log(store.io)
-// io.sockets.on('connection',function(socket){
-// socket.on('set')
-// })
-// model.on('set',function(path,obj){
-// console.log('***',path,obj)
-// })
+})
 
 
-// console.log(con)
 
 app.use(store.modelMiddleware());
 
 var derbyApp = require('./main');
-
+derbyApp.get('/', function(page, model, params, next) {
+  page.redirect('/main')
+});
 derbyApp.get('/main', function(page, model, params, next) {
   //asyc is used to tame the stream of callbacks
   //see: https://github.com/caolan/async
+
   async.waterfall([
-    function(callback) {
-      model.set('dbName', config.database.default.name);
-      db.driver.admin.listDatabases(function(e, dbs) {
-        callback(null, e, dbs);
-      });
-    }, function(e, dbs, callback) {
-      model.set('dbs', dbs);
-      //iterate trhough collection names
-      db.driver.collectionNames(function(e, names) {
-        callback(null, e, names);
-      });
-    }, function(e, names, callback){
-      console.log(names);
-      model.set('collections', names);
-      //iterate through database names
-      model.subscribe('dbs', function() {
-        callback(null);
-      });
-    }, function(callback) {
-      //iterate through collections
-      model.subscribe('collections', function() {
-        callback(null);
-      });
-    }, function(callback) {
-      //for each database, render to page;
-      model.subscribe('dbName', function(e, dbName) {
+
+  function(callback) {
+    model.set('dbName', config.database.default.name);
+    db.driver.admin.listDatabases(function(e, dbs) {
+      callback(null, e, dbs);
+    });
+  }, function(e, dbs, callback) {
+    model.set('dbs', dbs);
+    //iterate trhough collection names
+    db.driver.collectionNames(function(e, names) {
+      callback(null, e, names);
+    });
+  }, function(e, names, callback) {
+    // console.log(names);
+    names.forEach(function(el,i,list){
+      el.name=el.name.split('.')[1];
+    })
+    model.set('collections', names);
+    //iterate through database names
+    model.subscribe('dbs', function() {
+      callback(null);
+    });
+  }, function(callback) {
+    //iterate through collections
+    model.subscribe('collections', function() {
+      callback(null);
+    });
+  }, function(callback) {
+    //for each database, render to page;
+    model.subscribe('dbName', function(e, dbName) {
+            model.subscribe('collectionBox', function() {
         page.render();
+        // done();
       });
-    }
-  ]);
+      
+    });
+  }]);
 });
 
+// derbyApp.get('/main/collections', function(page, model, params, next){
+//   console.log('boom!')
+// })
 // derbyApp.on('changeDatabase',function(obj,obj){
 // console.log('HAHAHA')
 // })
@@ -105,7 +121,7 @@ app.use(express.static(__dirname + '/public'));
 // console.log(Object.keys(model.async))
 // console.log(model)
 app.get('/main', function(req, res) {
-  console.log(req.getModel().get('dbName'));
+  // console.log(req.getModel().get('dbName'));
 });
 app.get('/api.json', function(req, res) {
   db.driver.admin.listDatabases(function(e, dbs) {
@@ -123,7 +139,7 @@ app.get('/api/collections/:db.:name.json', function(req, res) {
   collection.find({}, {
     limit: 20
   }, function(e, docs) {
-    console.log('boo',docs);
+    // console.log('boo', docs);
     res.json(docs);
   });
 });
