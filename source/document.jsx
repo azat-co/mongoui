07e8bff4919b90304c82f42f86ee725a90abab1e
azat-co <hi@azat.co>
2016-04-14 17:10:03 -0700

    add changes

--- source/document.jsx
+++ source/document.jsx
@@ -1,4 +1,4 @@
-var {Navbar, NavItem, NavDropdown, Nav, MenuItem, PageHeader, Glyphicon, Badge, Button} = require('react-bootstrap')
+var {Navbar, NavItem, NavDropdown, Nav, MenuItem, PageHeader, Glyphicon, Badge, Button, Collapse} = require('react-bootstrap')
 let React = require('react')
 let request = require('request')
 let baseUrl = 'http://localhost:3001'
@@ -9,8 +9,7 @@ module.exports = React.createClass({
     return {expanded: false}
   },
   toggleExpand(){
-    let expanded = this.state.expanded
-    this.setState({expanded: !expanded})
+    this.setState({expanded: !this.state.expanded})
   },
   componentDidMount() {
     // request({url: `${baseUrl}/api/collections/${this.props.params.collectionName}`,
@@ -21,16 +20,16 @@ module.exports = React.createClass({
     //     this.setState({docs: body.docs})
     // })
   },
-  renderExpanded(){
-    return <div>{Object.keys(this.props.document).map((key)=>{
-      return <p key={key}>{key}: {this.props.document[key]}</p>
-    })}</div>
-  },
+
   render() {
     let document = this.props.document
     return  <div>
       <p key={document._id}><Button bsStyle="link" onClick={this.toggleExpand}>{document._id} <Badge><Glyphicon glyph="plus" /></Badge></Button></p>
-      {(this.state.expanded)? this.renderExpanded() : ''}
+      <Collapse in={this.state.expanded}>
+        <div>{Object.keys(this.props.document).map((key)=>{
+          return <p key={key}>{key}: {this.props.document[key]}</p>
+        })}</div>
+      </Collapse>
     </div>
   }
 })
