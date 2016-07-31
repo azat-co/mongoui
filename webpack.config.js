b3083925ed35d540dd83417748bed3758c6fd79b
Will Hart <hart.wl@gmail.com>
2016-07-31 13:20:02 +1000

    Ensure path is cross platform using path.join

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,10 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
     'webpack/hot/only-dev-server', // 'only' prevents reload on syntax errors
-    './source/app.jsx',
+    './source/app.jsx'
   ],
   output: {
-    path: __dirname + '/public/js',
+    path: path.join(__dirname, 'public/js'),
     filename: 'bundle.js',
     publicPath: '/js/'
   },
