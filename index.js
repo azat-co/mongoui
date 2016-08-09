0c93a0601fe593b61f815cb04e7016242399c290
Tom2277 <Tom2277@users.noreply.github.com>
2016-08-08 17:16:31 -0700

    add regex query ability

--- index.js
+++ index.js
@@ -70,8 +70,19 @@ app.get('/api/dbs/:dbName/collections/:collectionName', function(req, res, next)
   let query = {}
   try {
     query = JSON.parse(req.query.query)
+    //recognize and convert any regex queries from strings into regex objects
+    for (var prop in query){
+      if ((query[prop][0] == "R" && query[prop][1] == "/") //arbitrary letter 'R' used by this app
+        && (query[prop].length > 3)   //avoids a few corner cases
+        && ((query[prop][(query[prop].length - 1) ] == "/" ) || (query[prop][(query[prop].length - 2)] == "/") || (query[prop][query[prop].length - 3 ] == "/" )|| (query[prop][query[prop].length - 4 ] == "/"  ))
+      ){
+        var splitRegex = query[prop].split("/")
+        var makeRegex = new RegExp( splitRegex[1], splitRegex[2])
+        query[prop] = makeRegex
+      }
+    }
   } catch (error) {
-    return next(new Error('Invalind query, cannot parse it'))
+    return next(new Error('Invalid query, cannot parse it'))
   }
   if (query._id) {
     if (query._id['$in'] && Array.isArray(query._id.$in)) {
